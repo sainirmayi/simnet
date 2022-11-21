72fe648c06fd404c9e25c7fe206442e9a2c556a7
Kato Milis <kato.milis@student.kuleuven.be>
2022-11-21 21:52:30 +0100

    blast + uniprot

--- similaritynetworks/UniprotRetrieval/uniprot.py
+++ similaritynetworks/UniprotRetrieval/uniprot.py
@@ -8,7 +8,7 @@ def uniprot_retrieval(query_id):
     base = "https://rest.uniprot.org/uniprotkb/"
     query = "search?query="
     format = "&format=tsv"
-    columns = "&fields=accession,id,gene_names,sequence,organism_name,organism_id,protein_name"
+    columns = "&fields=accession,id,gene_primary,gene_names,sequence,organism_name,organism_id,protein_name,protein_families,xref_alphafolddb, xref_pdb, cc_function,ec,cc_pathway,ph_dependence,temp_dependence"
     result = requests.get(base + query + query_id + format + columns)
     df = pd.read_csv(io.StringIO(result.text), sep='\t')
     return df
@@ -16,7 +16,24 @@ def uniprot_retrieval(query_id):
 
 if __name__ == '__main__':
     blast_dataframe = pd.read_csv('../Blast/blast.csv')
-    proteins = blast_dataframe['Protein1'].append(blast_dataframe['Protein2']).drop_duplicates()
+    blast_proteins = blast_dataframe['Protein1'].append(blast_dataframe['Protein2']).drop_duplicates()
+    print(len(blast_proteins))
+
+    ssearch_dataframe = pd.read_csv('../FASTA/ssearch.csv')
+    ssearch_proteins = ssearch_dataframe['Protein1'].append(ssearch_dataframe['Protein2']).drop_duplicates()
+    print(len(ssearch_proteins))
+
+    fasta_dataframe = pd.read_csv('../FASTA/fasta.csv')
+    fasta_proteins = fasta_dataframe['Protein1'].append(fasta_dataframe['Protein2']).drop_duplicates()
+    print(len(fasta_proteins))
+
+    hmmer_dataframe = pd.read_csv('../Hmmer/outputTrimmed.csv')
+    hmmer_proteins = hmmer_dataframe['Protein1'].append(hmmer_dataframe['Protein2']).drop_duplicates()
+    print(len(hmmer_proteins))
+
+
+    proteins = blast_proteins.append(ssearch_proteins).append(hmmer_proteins).append(fasta_proteins).drop_duplicates()
+    print(proteins)
 
     df = pd.DataFrame()
     process = 0
